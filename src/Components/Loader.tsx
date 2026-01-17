9ff2883b0df5460607f5d6c912c6ed4d1f568055
Musiki8213 <sithomolamusiki@gmail.com>
2026-01-17 21:22:01 +0200

    feat: add reusable loader component with styles

--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -1,19 +1,7 @@
-
+import './Loader.css';
 
 export default function Loader() {
   return (
-    <div
-      id="loader"
-      style={{
-        display: "inline-block",
-        width: "20px",
-        height: "20px",
-        border: "3px solid #f3f3f3",
-        borderTop: "3px solid #E64A19",
-        borderRadius: "50%",
-        animation: "spin 1s linear infinite",
-        marginLeft: "10px",
-      }}
-    />
+    <div className="loader" />
   );
 }
