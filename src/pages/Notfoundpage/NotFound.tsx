62d8d41e2376d0c023e1085c87a27698186e09bf
Musiki8213 <sithomolamusiki@gmail.com>
2026-01-17 21:23:10 +0200

    style: add responsive not-found page layout

--- src/pages/Notfoundpage/NotFound.tsx
+++ src/pages/Notfoundpage/NotFound.tsx
@@ -1,14 +1,16 @@
-
 import { Link } from "react-router-dom";
+import './NotFound.css';
 
 export default function NotFound() {
   return (
-    <div id="notfound-page" style={{ textAlign: "center", padding: "50px" }}>
-      <h1 id="notfound-title" style={{ fontSize: "4em", color: "#E64A19", marginBottom: "20px" }}>404</h1>
-      <p id="notfound-message" style={{ fontSize: "1.5em", marginBottom: "30px" }}>Oops! Page not found.</p>
-      <Link to="/" id="notfound-home-link" style={{ padding: "10px 20px", backgroundColor: "#FF8F00", color: "#000", borderRadius: "5px", textDecoration: "none", fontWeight: "bold" }}>
-        Go to Landing Page
-      </Link>
-    </div>
+    <section id="notfound-container">
+      <div id="notfound-page">
+        <h1 id="notfound-title">404</h1>
+        <p id="notfound-message">Oops! This page doesn't exist.</p>
+        <Link to="/" id="notfound-home-link">
+          Go to Landing Page
+        </Link>
+      </div>
+    </section>
   );
 }
