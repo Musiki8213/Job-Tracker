682868d6f354115867ab0144a4bb129e05144af2
Musiki8213 <sithomolamusiki@gmail.com>
2026-01-17 21:21:35 +0200

    style: improve app layout and global responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,25 @@ import Register from "./pages/Registerpage/Register";
 import Home from "./pages/Homepage/Home";
 import JobDetails from "./pages/Jobpage/JobDetails";
 import NotFound from "./pages/Notfoundpage/NotFound";
-import { AuthContext } from "./Context/AuthContext";
-
+import { AuthProvider } from "./Context/AuthContext";
+import { ToastProvider } from "./Context/ToastContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/jobs/:id" element={<JobDetails />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ToastProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/jobs/:id" element={<JobDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </ToastProvider>
   );
 }
 
